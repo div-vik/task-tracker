24273062ff163cfc063a6561f7e696259fe1bf65
SidZhoop <siddharth@zhoop.in>
2023-08-25 13:33:50 +0530

    setup

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const navbar = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
